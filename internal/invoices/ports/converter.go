0fa884246914b99badd2d4fcfe13bc35d6068b88
ricardogrande-masmovil <ricardo.grandec@masmovil.com>
2025-05-18 14:18:49 +0200

    include query params to tool

--- internal/invoices/ports/converter.go
+++ internal/invoices/ports/converter.go
@@ -3,6 +3,7 @@ package ports
 import (
 	"encoding/json"
 	"errors"
+	"time"
 
 	domain "github.com/ricardogrande-masmovil/billing-mcp/internal/invoices/domain/model"
 )
@@ -10,6 +11,8 @@ import (
 var (
 	ErrInvalidInvoice      = errors.New("invalid invoice")
 	ErrInvalidInvoices     = errors.New("invalid invoices")
+	ErrInvalidStatusCriteria = errors.New("invalid status criteria")
+	ErrInvalidDateCriteria = errors.New("invalid date criteria")
 )
 
 type Converter struct{}
@@ -20,7 +23,7 @@ func NewConverter() Converter {
 
 func (c Converter) ConvertDomainInvoiceToJsonInvoice(domainInvoice domain.Invoice) ([]byte, error) {
 	mcpInvoice := Invoice{
-		ID:               string(domainInvoice.ID),
+		ID:               domainInvoice.ID.String(),
 		AmountWithoutTax: int(domainInvoice.TotalAmountWithoutTax),
 		AmountWithTax:    int(domainInvoice.TotalAmountWithTax),
 		Status:           string(domainInvoice.Status),
@@ -38,7 +41,7 @@ func (c Converter) ConvertDomainInvoicesToJsonInvoices(domainInvoices domain.Inv
 	mcpInvoices := make([]Invoice, len(domainInvoices))
 	for i, domainInvoice := range domainInvoices {
 		mcpInvoices[i] = Invoice{
-			ID:               string(domainInvoice.ID),
+			ID:               domainInvoice.ID.String(),
 			AmountWithoutTax: int(domainInvoice.TotalAmountWithoutTax),
 			AmountWithTax:    int(domainInvoice.TotalAmountWithTax),
 			Status:           string(domainInvoice.Status),
@@ -51,4 +54,41 @@ func (c Converter) ConvertDomainInvoicesToJsonInvoices(domainInvoices domain.Inv
 		return nil, ErrInvalidInvoices
 	}
 	return jsonData, nil
+}
+
+func (c Converter) ConvertRequestArgsToCriteria(args map[string]any) (domain.Criteria, error) {
+	criteria := domain.Criteria{}
+
+	paramStatus, ok := (args["status"]).(string)
+	if !ok {
+		return criteria, ErrInvalidStatusCriteria
+	}
+
+	if status, err := domain.GetStatusFromString(paramStatus); err != nil {
+		return criteria, err
+	} else {
+		criteria.Status = status
+	}
+
+	paramStartDate, ok := (args["issueDateFrom"]).(string)
+	if !ok {
+		return criteria, ErrInvalidDateCriteria
+	}
+	if startDate, err := time.Parse(time.RFC3339, paramStartDate); err != nil {
+		return criteria, err
+	} else {
+		criteria.IssueDateFrom = startDate
+	}
+
+	paramEndDate, ok := (args["issueDateTo"]).(string)
+	if !ok {
+		return criteria, ErrInvalidDateCriteria
+	}
+	if endDate, err := time.Parse(time.RFC3339, paramEndDate); err != nil {
+		return criteria, err
+	} else {
+		criteria.IssueDateTo = endDate
+	}
+
+	return criteria, nil
 }
--- internal/invoices/ports/mcp.go
+++ internal/invoices/ports/mcp.go
@@ -10,13 +10,20 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+type InvoiceService interface {
+	GetInvoiceByID(id domain.InvoiceID) (domain.Invoice, error)
+	GetInvoicesByCriteria(criteria domain.Criteria) (domain.Invoices, error)
+}
+
 type controller struct {
+	service   InvoiceService
 	converter Converter
 	logger    zerolog.Logger
 }
 
-func NewController() controller {
+func NewController(service InvoiceService) controller {
 	return controller{
+		service:   service,
 		converter: NewConverter(),
 		logger:    log.With().Str("module", "invoicesMcpController").Logger(),
 	}
@@ -26,7 +33,7 @@ func (c controller) GetInvoice(ctx context.Context, request mcp.CallToolRequest)
 	c.logger.Info().Msg("Processing request in GetInvoice tool")
 
 	mockInvoice := domain.Invoice{
-		ID:                    domain.InvoiceID("invoice-123"),
+		ID:                    domain.NewInvoiceID(),
 		TotalAmountWithoutTax: 100,
 		TotalAmountWithTax:    120,
 		Status:                domain.InvoiceStatusPaid,
@@ -47,9 +54,11 @@ func (c controller) GetInvoice(ctx context.Context, request mcp.CallToolRequest)
 func (c controller) GetInvoices(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
 	c.logger.Info().Msg("Processing request in GetInvoices tool")
 
+	criteria, err := c.converter.ConvertRequestArgsToCriteria(request.Params.Arguments)
+
 	mockInvoices := domain.Invoices{
 		domain.Invoice{
-			ID:                    domain.InvoiceID("invoice-123"),
+			ID:                    domain.NewInvoiceID(),
 			TotalAmountWithoutTax: 100,
 			TotalAmountWithTax:    120,
 			Status:                domain.InvoiceStatusPaid,
@@ -57,13 +66,19 @@ func (c controller) GetInvoices(ctx context.Context, request mcp.CallToolRequest
 			DueDate:               time.Date(2025, 1, 31, 0, 0, 0, 0, time.UTC),
 		},
 		domain.Invoice{
-			ID:                    domain.InvoiceID("invoice-456"),
+			ID:                    domain.NewInvoiceID(),
 			TotalAmountWithoutTax: 200,
 			TotalAmountWithTax:    240,
 			Status:                domain.InvoiceStatusUnpaid,
 			IssueDate:             time.Date(2025, 2, 1, 0, 0, 0, 0, time.UTC),
 			DueDate:               time.Date(2025, 2, 28, 0, 0, 0, 0, time.UTC),
 		},
+	}
+
+	_, err = c.service.GetInvoicesByCriteria(criteria)
+	if err != nil {
+		c.logger.Error().Err(err).Msg("Failed to fetch invoices by criteria")
+		return nil, err
 	}
 
 	jsonData, err := c.converter.ConvertDomainInvoicesToJsonInvoices(mockInvoices)
