ea58f364c311610c3b5bba84d27fbeb336bf64be
ricardogrande-masmovil <ricardo.grandec@masmovil.com>
2025-05-18 14:17:46 +0200

    commons gorm package

---
+++ internal/commons/persistence/gorm/base_model.go
@@ -0,0 +1,22 @@
+package gorm
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type BaseModel struct {
+	ID        uuid.UUID      `gorm:"type:uuid;primaryKey;" json:"id"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at"`
+}
+
+func (bm *BaseModel) BeforeCreate(tx *gorm.DB) (err error) {
+	if bm.ID == uuid.Nil {
+		bm.ID = uuid.New()
+	}
+	return
+}
---
+++ internal/commons/persistence/gorm/gorm_client.go
@@ -0,0 +1,55 @@
+package gorm
+
+import (
+	"errors"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	gormlogger "gorm.io/gorm/logger"
+)
+
+// ZerologGormWriter is a custom writer to bridge GORM's logger with zerolog.
+type ZerologGormWriter struct {
+	logger zerolog.Logger
+}
+
+// NewZerologGormWriter creates a new writer for GORM.
+func NewZerologGormWriter(logger zerolog.Logger) *ZerologGormWriter {
+	return &ZerologGormWriter{logger: logger}
+}
+
+// Printf implements gormlogger.Writer interface
+func (z *ZerologGormWriter) Printf(format string, args ...interface{}) {
+	z.logger.Printf(format, args...)
+}
+
+// NewDB initializes and returns a new Gorm DB instance for PostgreSQL.
+// dsn is the Data Source Name for connecting to the PostgreSQL database.
+// Example DSN: "host=localhost user=gorm password=gorm dbname=gorm port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+func NewDB(dsn string) (*gorm.DB, error) {
+	gormLog := log.With().Str("component", "gorm").Logger()
+
+	gormDBLogger := gormlogger.New(
+		NewZerologGormWriter(gormLog),
+		gormlogger.Config{
+			SlowThreshold:             200,
+			LogLevel:                  gormlogger.Info,
+			IgnoreRecordNotFoundError: false,
+			Colorful:                  false,
+		},
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: gormDBLogger,
+	})
+
+	if err != nil {
+		gormLog.Error().Err(err).Msg("Failed to connect to PostgreSQL database")
+		return nil, errors.New("failed to connect to PostgreSQL database")
+	}
+
+	gormLog.Info().Msg("PostgreSQL database connection established.")
+	return db, nil
+}
